9eb1573c52163b5cac4bb972b9a0530b7a0bc27b
kauanzeraaa <piacentekauan@gmail.com>
2026-03-09 23:16:05 -0300

    adding_vite_configs

--- epi_system/src/components/Header.vue
+++ epi_system/src/components/Header.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import router from '../router';
-
 </script>
 
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/resources">Resources</router-link>
-    <router-link to="/contact">Contact</router-link>
+    <router-link to="/">Home </router-link>
+    <router-link to="/resources"> Resources </router-link>
+    <router-link to="/contact"> Contact </router-link>
   </nav>
 </template>
